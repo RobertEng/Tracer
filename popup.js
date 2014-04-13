fd977b3dca7357f60caf505544e18a6f50b0fbc8
m3hr <mehrzadfar@gmail.com>
2014-04-12 19:05:55 -0700

    Add form

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ chrome.storage.sync.get("projArray", function(data){
   if(chrome.runtime.lastError){
     //No projArray in storage. Therefore, make new one.
     var projArray = new Array();
-    chrome.storage.sync.set({"projArray"}:projArray);
+    chrome.storage.sync.set({"projArray":projArray});
     return;
   }
 
@@ -12,24 +12,30 @@ chrome.storage.sync.get("projArray", function(data){
 
 
 
-document.getElementById("add_Project").addEventListener('click',addProject());
+document.getElementById("add-project").addEventListener('click',addProject());
 
 
-function addProject(){
-  projArray = chrome.storage.sync.get("projArray");
-  var singleUrlList = new Array();
-  var randId = Math.floor((Math.random()*1000000));
-  //Check if id is same as later other ids. highly improbable, but possible  
-  projObj = {projName:"New Project", urlList:singleUrlList, id:randId}
-  projArray.push(projObj);
-  chrome.storage.sync.set({"projArray"}:projArray);
+// function addProject(){ 
+//   projArray = chrome.storage.sync.get("projArray");
+//   var singleUrlList = new Array();
+//   var randId = Math.floor((Math.random()*1000000));
+//   //Check if id is same as later other ids. highly improbable, but possible  
+//   projObj = {projName:"New Project", urlList:singleUrlList, id:randId}
+//   projArray.push(projObj);
+//   chrome.storage.sync.set({"projArray"}:projArray);
 
-  //Change html to reflect new project here or...
-  //add listener somewhere else which listens for a change in the chrome storage
+//   //Change html to reflect new project here or...
+//   //add listener somewhere else which listens for a change in the chrome storage
 
+// }
+
+function addProject() {
+  var form = document.createElement("form");
+  form.innerHTML = 'name: <input type="text" name="name">';
+  useDiv = document.getElementById("orig-item");
+  useDiv.parentNode.insertBefore(form, useDiv);
 }
 
-  var projArray = [0, 1, 2, 3, 4];
     for (var i = 0; i < projArray.length; i++) {
       var item = document.createElement("tr");
       item.innerHTML = '<td>'+'<a href="">'+projArray[i]+'</a>'+'</td>' + '<td>record</td>';
